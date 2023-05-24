019a7507ee2b5544a652b0b9b1732022be1cd0bd
Gleb Gorelov <g.gorelov@haulmont.com>
2023-05-24 11:45:30 +0400

    Finished

---
+++ src/main/java/com/company/jmixpm/screen/projecttasksbrowser/ProjectTasksBrowser.java
@@ -0,0 +1,28 @@
+package com.company.jmixpm.screen.projecttasksbrowser;
+
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.Task;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Nullable;
+
+@UiController("ProjectTasksBrowser")
+@UiDescriptor("project-tasks-browser.xml")
+public class ProjectTasksBrowser extends Screen {
+    @Autowired
+    private CollectionLoader<Task> tasksDl;
+
+    public void setProject(@Nullable Project project) {
+        if (project != null) {
+            tasksDl.setParameter("projectId", project.getId());
+        } else {
+            tasksDl.removeParameter("projectId");
+        }
+
+        tasksDl.load();
+    }
+}
